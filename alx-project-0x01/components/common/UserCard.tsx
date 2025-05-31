47d6ee7d8aba7b3d56aae549e9e4888e3c9f99c9
mojet66 <oladosuemmanuel2017@gmail.com>
2025-05-31 22:01:52 +0100

    making second corrections in task 3

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { UserProps } from "@/interfaces";
 
-const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
+const UserCard: React.FC<UserProps> = ({
+  id,
+  name,
+  username,
+  address,
+  email,
+  phone,
+  website,
+  company,
+}) => {
   return (
     <div className="bg-white shadow-md hover:shadow-lg mx-auto my-6 p-6 rounded-lg max-w-xl transition-shadow duration-300">
-      <h2 className="mb-2 font-bold text-blue-700 text-2xl">{user.name}</h2>
-      <p className="text-gray-600">@{user.username}</p>
-      <p className="text-gray-600">{user.email}</p>
-      <p className="text-gray-600">{user.phone}</p>
-      <p className="mb-2 text-gray-600">{user.website}</p>
+      <h2 className="mb-2 font-bold text-blue-700 text-2xl">{name}</h2>
+      <p className="text-gray-600">@{username}</p>
+      <p className="text-gray-600">{email}</p>
+      <p className="text-gray-600">{phone}</p>
+      <p className="mb-2 text-gray-600">{website}</p>
 
       <div className="text-gray-500 text-sm">
         <h4 className="font-semibold">Address:</h4>
         <p>
-          {user.address.street}, {user.address.suite}, {user.address.city}
+          {address.street}, {address.suite}, {address.city}
         </p>
-        <p>Zip: {user.address.zipcode}</p>
+        <p>Zip: {address.zipcode}</p>
 
         <h4 className="mt-2 font-semibold">Company:</h4>
-        <p>{user.company.name}</p>
-        <p className="italic">"{user.company.catchPhrase}"</p>
+        <p>{company.name}</p>
+        <p className="italic">"{company.catchPhrase}"</p>
       </div>
     </div>
   );
